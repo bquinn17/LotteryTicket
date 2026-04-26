23e2abc250fcc1866a3a14a46d72fa5be5362631
Bryan Quinn <bryan.quinn17@gmail.com>
2026-04-25 21:28:57 -0400

    Refactor calculate value and udpate game rules

--- src/CalculateValue.js
+++ src/CalculateValue.js
@@ -12,133 +12,98 @@
  *  www.khanacademy.org/math/probability/random-variables-topic/expected-value/v/expected-value-profit-lottery-ticket
  */
 
-var pricePerTicket;
-var numberOfTickets;
-var PowerBallOrMegaMillions;
+const GAMES = {
+    p: {
+        // Powerball: 1 / ((69 choose 5) * (26 choose 1))
+        jackpotOdds: 1 / 292201338,
+        // See PBWinningTable.PNG for the per-prize breakdown that produces this constant.
+        // Sum of P(prize) * (prize - ticketPrice) for every non-jackpot prize tier.
+        otherPrizeEV: 0.23944579142,
+        // 1 - P(winning any prize)
+        loseProbability: 0.97368,
+        // Jackpot resets to this value after a winner
+        resetJackpot: 40_000_000,
+    },
+    m: {
+        // MegaMillions: 1 / ((75 choose 5) * (15 choose 1))
+        jackpotOdds: 1 / 258890850,
+        // See MMWinningTable.PNG for the per-prize breakdown that produces this constant.
+        otherPrizeEV: 0.10623350747,
+        loseProbability: 0.930990,
+        resetJackpot: 40_000_000,
+    },
+};
 
-function formatJackpotForDisplay(jackpotValue){
-    if (jackpotValue >= 1000000000){
-        return (jackpotValue / 1000000000).toFixed(1).replace(/\.0$/, "") + " Billion";
+function formatJackpotForDisplay(jackpotValue) {
+    if (jackpotValue >= 1_000_000_000) {
+        return (jackpotValue / 1_000_000_000).toFixed(1).replace(/\.0$/, "") + " Billion";
     }
-    return (jackpotValue / 1000000).toFixed(1).replace(/\.0$/, "") + " Million";
+    return (jackpotValue / 1_000_000).toFixed(1).replace(/\.0$/, "") + " Million";
 }
 
-function oddsOfWinningJackpot(){
-    if (PowerBallOrMegaMillions == "p"){
-        return 1/292201338.00; // 1 / ((69 choose 5) * (26 choose 1))
-    } else {
-        return 1/258890850.00; // 1 / ((75 choose 5) * (15 choose 1))
-    }
+function noPrizeEV(game, pricePerTicket) {
+    return game.loseProbability * (-1 * pricePerTicket);
 }
 
-function otherPrizeWon(){
-    //Expected value of winning a prize other than the jackpot
-    //Since this is constant we can calculate it ahead of time
-    //Please note that these odds are exclusive, and do not
-    //  trickled down. Meaning that the odds represent the chance
-    //  of that single event happening, and nothing else. For example
-    //  the odds of matching one red ball, do not include the odds of
-    //  winning another prize that includes matching one red ball.
+// TODO: dead code path. Wire this back into calculateValue once the
+//   split-the-pot model is finished. See numberOfPlayers / getLastJackpot below.
+function oddsOfSplittingThePot(game, jackpot, pricePerTicket) {
+    // Returns a value that represents the probability of the size of your
+    // jackpot after accounting for the probability of splitting the pot
+    // between multiple winners.
+    const numOfPlayers = numberOfPlayers(game, jackpot, getLastJackpot(game), pricePerTicket);
 
-    if (PowerBallOrMegaMillions == "p"){ //Powerball
-        //See PBWinningTable.PNG for ways to win
-        //1 in 11,688,053.52 * ($1,000,000 - $2) = 0.08555727421
-        //1 in 913,129.18    * ($50,000 - $2)    = 0.05475457481
-        //1 in 36,525.17     * ($100 - $2)       = 0.00268308128
-        //1 in 14,494.11     * ($100 - $2)       = 0.0067613672
-        //1 in 579.76        * ($7 - $2)         = 0.00862425831
-        //1 in 701.33        * ($7 - $2)         = 0.00712931145
-        //1 in 91.98         * ($4 - $2)         = 0.02174385736
-        //1 in 38.32         * ($4 - $2)         = 0.0521920668
-        //add them all together                  = 0.23944579142
-        return 0.23944579142;
+    // The odds that someone else will win the jackpot given that you have won.
+    let odds = game.jackpotOdds * numOfPlayers;
 
-    } else { //MegaMillions
-        //See MMWinningTable.PNG for ways to win
-        //1 in 18,492,203.57 * ($1,000,000 - $1) = 0.0540767895
-        //1 in 739,688.14    * ($5,000 - $1)     = 0.00675825355
-        //1 in 52,834.87     * ($500 - $1)       = 0.00944452025
-        //1 in 10,720.12     * ($50 - $1)        = 0.00457084435
-        //1 in 766.72        * ($5 - $1)         = 0.00521702838
-        //1 in 472.95        * ($5 - $1)         = 0.00845755365
-        //1 in 56.47         * ($2 - $1)         = 0.01770851779
-        //1 in 21.39         * ($1 - $1)         = 0.0
-        //add them all together                  = 0.10623350747
-        return 0.10623350747;
-    }
-}
-
-function noPrizeWon(){
-    // 1 - the odds of winning any other prize
-    var chanceOfLosing;
-    if (PowerBallOrMegaMillions == "p"){
-        chanceOfLosing = 0.97368;
-    }else {
-        chanceOfLosing = 0.930990;
-    }
-    return chanceOfLosing * (-1 * pricePerTicket);
-}
-
-function oddsOfSplittingThePot(jackpot){
-    //Returns a value that represents the probability of the size of your
-    //jackpot after accounting for the probability of splitting the pot
-    //between multiple winners
-    //TODO this feature is not yet ready to be implemented
-    var numOfPlayers = numberOfPlayers(jackpot, getLastJackpot());
-
-    //the odds that someone else will win the jackpot given that you have won
-    var odds = oddsOfWinningJackpot() * numOfPlayers;
-
-    var numberOfWinners = 1;
-    while (odds > 0.00001){ //value is basically negligible past this point
-        odds = odds * Math.pow(oddsOfWinningJackpot(), numberOfWinners);
+    let numberOfWinners = 1;
+    while (odds > 0.00001) { // value is basically negligible past this point
+        odds = odds * Math.pow(game.jackpotOdds, numberOfWinners);
         numberOfWinners += 1;
     }
 
-    return jackpot; //- (jackpot * odds);
+    return jackpot; // - (jackpot * odds);
 }
 
-function numberOfPlayers(thisJackpot, lastJackpot){
-    //Calculate the number of tickets sold
-    if (lastJackpot > thisJackpot){ //jackpot was won
-        lastJackpot = thisJackpot - 40000000; //jackpot resets to 40 Million
+function numberOfPlayers(game, thisJackpot, lastJackpot, pricePerTicket) {
+    // Calculate the number of tickets sold.
+    if (lastJackpot > thisJackpot) { // jackpot was won
+        lastJackpot = thisJackpot - game.resetJackpot;
     }
-    var ticketSales = thisJackpot - lastJackpot;
-    //TODO account for PowerPlay and Megaplier
+    const ticketSales = thisJackpot - lastJackpot;
+    // TODO account for PowerPlay and Megaplier
     return ticketSales / pricePerTicket;
 }
 
-function getLastJackpot() {
-    //TODO request jackpot from the last drawing
-    if (PowerBallOrMegaMillions == "p") {
-        return 67_000_000;
-    } else {
-        return 40_000_000;
-    }
+function getLastJackpot(game) {
+    // TODO request jackpot from the last drawing
+    return game.resetJackpot;
 }
 
-function setValuesOnPage(expectedValue) {
-    document.getElementById(PowerBallOrMegaMillions + "_value").innerHTML += expectedValue.toFixed(2);
+function setValuesOnPage(gameKey, expectedValue) {
+    document.getElementById(gameKey + "_value").innerHTML += expectedValue.toFixed(2);
 }
 
-function calculateValue(P_or_M, estimatedJackpot, price, count){
-    PowerBallOrMegaMillions = P_or_M;
-    var jackpotValue = Number(estimatedJackpot);
-    if (!Number.isFinite(jackpotValue) || jackpotValue < 0){
+function calculateValue(gameKey, estimatedJackpot, pricePerTicket, numberOfTickets) {
+    const game = GAMES[gameKey];
+
+    let jackpotValue = Number(estimatedJackpot);
+    if (!Number.isFinite(jackpotValue) || jackpotValue < 0) {
         jackpotValue = 0;
     }
 
-    document.getElementById(P_or_M + "_jackpot").innerHTML += formatJackpotForDisplay(jackpotValue);
-    document.getElementById(P_or_M + "_price").innerHTML += price.toFixed(2);
-    pricePerTicket = price;
-    numberOfTickets = count;
+    document.getElementById(gameKey + "_jackpot").innerHTML += formatJackpotForDisplay(jackpotValue);
+    document.getElementById(gameKey + "_price").innerHTML += pricePerTicket.toFixed(2);
 
-    var jackpotAfterSplit = oddsOfSplittingThePot(jackpotValue);
+    // TODO: numberOfTickets is accepted for future use (multi-ticket EV scaling)
+    //   but not yet incorporated into the calculation.
 
-    var expectedValue =
-        (jackpotAfterSplit * oddsOfWinningJackpot()) //expected profit of jackpot
-        + otherPrizeWon() //expected profit of other prize
-        + noPrizeWon(); //expected profit of losing
+    // TODO: oddsOfSplittingThePot is unfinished — once ready, replace
+    //   jackpotValue below with oddsOfSplittingThePot(game, jackpotValue, pricePerTicket).
+    const expectedValue =
+        (jackpotValue * game.jackpotOdds)  // expected profit of jackpot
+        + game.otherPrizeEV                 // expected profit of other prize
+        + noPrizeEV(game, pricePerTicket);  // expected profit of losing
 
-    setValuesOnPage(expectedValue);
+    setValuesOnPage(gameKey, expectedValue);
 }
